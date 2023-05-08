faf17cdd993650358c6425529eac44b7aae6624f
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-08 12:15:38 +0530

    Add files via upload

---
+++ staticReadOnly.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Office
+{
+    
+    static readonly int lwp;
+    
+    static Office()
+    {
+
+        lwp = 1;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(lwp );
+
+        Console.ReadLine();
+
+    }
+}

c70a0d1059a4765e8c2cabc13d5793584ffc5c87
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-12 16:16:17 +0530

    Add files via upload

---
+++ Rough/Day7Collection/Day7Collection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+
+
+namespace Day7Collection
+{
+    class DataEntryContact
+    {
+        ArrayList obj = new ArrayList();
+        public void AddData()
+        {
+            obj.Add("Deepa");
+            obj.Add("Deepak");
+
+            foreach (var temp in obj) {
+                Console.WriteLine(temp);
+            }
+        }
+        public void deleteData()
+        {
+            //obj.Remove
+        }
+        public static void Main(string[] args)
+        {
+            DataEntryContact obj=new DataEntryContact();
+            obj.AddData();
+            Console.ReadLine();
+        }
+
+    }
+}

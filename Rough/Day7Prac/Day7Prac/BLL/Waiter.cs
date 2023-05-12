a0ec237528c9fa674fdb67c7ebbea6f83243d80a
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-12 16:18:17 +0530

    Add files via upload

---
+++ Rough/Day7Prac/Day7Prac/BLL/Waiter.cs
@@ -0,0 +1,24 @@
+﻿using Day7Prac.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Prac.BLL
+{
+    public class Waiter
+    {
+        public Menu FoodDetails()
+        {
+            Menu dish = new Menu();
+            Console.WriteLine("Enter dish name");
+            string n=Console.ReadLine();
+            dish.Name = n;
+            Console.WriteLine("Enter dish price");
+            int pri = int.Parse(Console.ReadLine());
+            dish.Price=pri;
+            return dish;
+        }
+    }
+}
---
+++ Rough/Day7Prac/Day7Prac/EntryPoint/Customer.cs
@@ -0,0 +1,22 @@
+﻿using Day7Prac.Model;
+using Day7Prac.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7Prac.EntryPoint
+{
+    class Customer
+    {
+        static void Main(String[] args)
+        {
+            Waiter obj=new Waiter();
+            Menu temp=obj.FoodDetails();
+            Console.WriteLine("Food Name \tFood Price");
+            Console.WriteLine(temp.Name + "\t" + temp.Price);
+            Console.ReadLine();
+        }
+    }
+}

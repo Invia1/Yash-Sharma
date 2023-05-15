e200cfee318600bbbb68913003b8d8eb0d4ad337
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-15 16:37:32 +0530

    Add files via upload

---
+++ Rough/Test1Practise/Collection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1Practise
+{
+    internal class Collection
+    {
+        ArrayList arrList = new ArrayList();
+        public void AddData(object T)
+        {
+            arrList.Add(T);
+        }
+        public void RemoveData(object T)
+        {
+            arrList.Remove(T);
+        }
+        public void SearchData(object T)
+        {
+            arrList.Contains(T);
+        }
+        public void fetchAll()
+        {
+            foreach (object T in arrList)
+            {
+                Console.WriteLine(T);
+            }
+        }
+
+    }
+    class Calling
+    {
+        public static void Main(string[] args)
+        {
+            Collection c = new Collection();
+            c.AddData(4);
+            c.fetchAll();
+            Console.ReadLine();
+        }
+        
+    }
+}
